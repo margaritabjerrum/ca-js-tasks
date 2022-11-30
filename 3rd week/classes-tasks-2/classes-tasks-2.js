9846c8bfa848aa6b62c547bac4d6507ab511dc0f
margaritabjerrum <margarita.juskyte@gmail.com>
2022-11-30 15:34:45 +0200

    Week3: Classes-tasks-2: Task 1 completed

--- 3rd week/classes-tasks-2/classes-tasks-2.js
+++ 3rd week/classes-tasks-2/classes-tasks-2.js
@@ -1,6 +1,57 @@
 console.groupCollapsed('1. - https://edabit.com/challenge/ZngT4zDckDugt2JGY');
 {
-  // ... code
+  class Player {
+    name;
+    age;
+    height;
+    weight;
+
+    constructor(name, age, height, weight) {
+      this.name = name,
+        this.age = age,
+        this.height = height,
+        this.weight = weight
+    }
+
+    getAge() {
+      return `${this.name} is age ${this.age}`;
+    }
+
+    getHeight() {
+      return `${this.name} is ${this.height}cm`;
+    }
+
+    getWeight() {
+      return `${this.name} weighs ${this.weight}kg`;
+    }
+  }
+
+  const player1 = new Player('Patrick Mahomes', 24, 188, 104);
+  const player2 = new Player('Jimmy Garoppolo', 28, 188, 102);
+  const player3 = new Player('Julio Jones', 31, 191, 100);
+
+  console.table(player1);
+  console.table(player2);
+  console.table(player3);
+
+  console.log('---age---');
+
+  console.log(player1.getAge());
+  console.log(player2.getAge());
+  console.log(player3.getAge());
+
+  console.log('---height---');
+
+  console.log(player1.getHeight());
+  console.log(player2.getHeight());
+  console.log(player3.getHeight());
+
+  console.log('---weight---');
+
+  console.log(player1.getWeight());
+  console.log(player2.getWeight());
+  console.log(player3.getWeight());
+
 }
 console.groupEnd();
 
