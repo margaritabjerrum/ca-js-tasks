fa4b954963c4a2b4b2eea1a26d270edf12ab52f6
margaritabjerrum <margarita.juskyte@gmail.com>
2022-12-01 14:16:39 +0200

    Week3: Classes-tasks-2: Task 3 completed

--- 3rd week/classes-tasks-2/classes-tasks-2.js
+++ 3rd week/classes-tasks-2/classes-tasks-2.js
@@ -98,7 +98,27 @@ console.groupEnd();
 
 console.groupCollapsed('3. - https://edabit.com/challenge/kGLhgwGaLJsCMS7wS');
 {
+  class Employee {
+    constructor (firstname, lastname) {
+      this.firstname = firstname,
+      this.lastname = lastname,
+      this.fullname = (`${this.firstname} ${this.lastname}`),
+      this.email = `${this.firstname}.${this.lastname}@company.com`.toLowerCase()
+    }
+  }
 
+  const emp1 = new Employee("John", "Smith");
+  const emp2 = new Employee("Mary",  "Sue");
+  const emp3 = new Employee("Antony", "Walker");
+  const emp4 = new Employee("Joshua", "Senoron");
+
+  console.table(emp1);
+
+  console.table(emp2);
+
+  console.table(emp3);
+
+  console.table(emp4);
 }
 console.groupEnd();
 
