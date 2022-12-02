7e0e62c9dc756b6bafec5ef7c5e87447296096c5
margaritabjerrum <margarita.juskyte@gmail.com>
2022-12-02 14:19:39 +0200

    Week3: Classes-tasks-2: Task 7 completed

--- 3rd week/classes-tasks-2/classes-tasks-2.js
+++ 3rd week/classes-tasks-2/classes-tasks-2.js
@@ -200,7 +200,43 @@ console.groupEnd();
 
 console.groupCollapsed('7. - https://edabit.com/challenge/HKmJFmZZCX53ff4ke');
 {
-  // ... code
+  class IceCream {
+    constructor(flavor, numSprinkles) {
+      this.flavor = flavor
+      this.numSprinkles = numSprinkles
+    }
+  }
+
+  function sweetestIcecream(arr) {
+    const flavors = {
+        'Plain': 0,
+        'Vanilla': 5,
+        'ChocolateChip': 5,
+        'Strawberry': 10,
+        'Chocolate': 10
+      };
+
+    const totalSweetness =  arr.map(({flavor, numSprinkles}) => flavors[flavor] + numSprinkles);
+    return Math.max(...totalSweetness);
+  }
+
+  let ice1 = new IceCream("Chocolate", 13);
+  let ice2 = new IceCream("Vanilla", 0);
+  let ice3 = new IceCream("Strawberry", 7);
+  let ice4 = new IceCream("Plain", 18);
+  let ice5 = new IceCream("ChocolateChip", 3);
+  let ice6 = new IceCream("Chocolate", 23);
+  let ice7 = new IceCream("Strawberry", 0);
+  let ice8 = new IceCream("Plain", 34);
+  let ice9 = new IceCream("Plain", 81);
+  let ice10 = new IceCream("Vanilla", 12);
+
+  console.log(sweetestIcecream([ice1, ice2, ice3, ice4, ice5]));
+  console.log(sweetestIcecream([ice7, ice10, ice1, ice6, ice8, ice10, ice2, ice2]));
+  console.log(sweetestIcecream([ice10, ice10, ice6, ice8, ice4]));
+  console.log(sweetestIcecream([ice2, ice10, ice6, ice9, ice7]));
+  console.log(sweetestIcecream([ice10, ice6, ice4, ice1, ice7, ice8, ice6]));
+
 }
 console.groupEnd();
 
