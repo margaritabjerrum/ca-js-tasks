a64a75d56f7bc73588e0efc20fcd6196c9b1b6a9
margaritabjerrum <margarita.juskyte@gmail.com>
2022-12-15 16:08:34 +0200

    Week5: Classes-tasks-3: Task 2 completed

--- 5th week/classes-tasks-3/classes-tasks-3.js
+++ 5th week/classes-tasks-3/classes-tasks-3.js
@@ -98,7 +98,116 @@ console.groupEnd();
 
 console.groupCollapsed('https://edabit.com/challenge/ifDM26p25bqS8EsFu');
 {
-  // ... code
+  class Player {
+    #hp;
+    #maxHp;
+    #en;
+    #maxEn;
+    armor;
+    name;
+
+    constructor({
+      name,
+      hp,
+      en,
+      armor
+    })
+
+    {
+      this.hp = hp;
+      this.#maxHp = hp;
+      this.en = en;
+      this.#maxEn = en;
+      this.armor = armor;
+      this.name = name;
+    }
+
+    set hp(value) {
+      this.#hp = value
+    }
+
+    get hp() {
+      return this.#hp;
+    }
+
+    set en(value) {
+      if (this.en < 0 || this.en > this.#maxEn) {
+        console.error(`HP cannot be less than 0 or more than player's MaxHP`);
+        return;
+      } 
+
+      this.#en = value;
+    }
+
+    get en() {
+      return this.#en;
+    }
+    
+    get hpPerc() {
+      return Number((100 * this.#hp / this.#maxHp).toFixed(2));
+    }
+
+    learnSkill(skillName, {damage, penetration, heal, cost, desc}) {
+      this[skillName] = function(frenemy) {
+        const effectiveArmor = frenemy.armor - penetration;
+
+        if (this.#en < cost) return `${this.name} attempted to use ${skillName}, but didn't have enough energy!`;
+        this.en -= cost;
+
+        const damageAmountDone = Number((damage * ((100 - effectiveArmor)/100)).toFixed(2));
+
+        frenemy.hp -= damageAmountDone;
+
+        const healthBeforeHeal = this.hp;
+        this.hp += heal;
+        const healthAfterHeal = this.hp - healthBeforeHeal;
+
+        return ( `${this.name} used ${skillName}, ${desc}, against ${frenemy.name}, doing ${damageAmountDone} damage!`
+          + (heal !== 0 ? ` ${this.name} healed for ${heal} health!` : '')
+          + (frenemy.hp <= 0 ? ` ${frenemy.name} died.` : ` ${frenemy.name} is at ${frenemy.hpPerc}% health.`)
+        )      
+      }
+    }
+  }
+  
+ 
+
+const alice = new Player({name: "Alice", hp: 110, en: 50, armor: 10});
+const bob = new Player({name: "Bob", hp: 100, en: 60, armor: 20});
+
+console.log(alice)
+console.log(bob)
+
+alice.learnSkill('fireball',{
+  damage: 23,
+  penetration: 1.2,
+  heal: 0,
+  cost: 15,
+  desc: 'a firey magical attack'
+});
+
+console.log(alice.fireball(bob));
+
+bob.learnSkill('superbeam',{
+	damage:200,
+  penetration:50,
+  heal:50,
+  cost:75,
+  desc: "an overpowered attack, pls nerf"
+})
+
+console.log(bob.superbeam(alice));
+
+console.log("\n----\nBut wait! Bob's back in action!")
+bob.learnSkill('Strike',{
+	damage:118,
+  penetration:4,
+  heal:5,
+  cost:20,
+  desc: "an attack that basically ends the game (gg)"
+})
+
+console.log(bob.Strike(alice));
+
 }
 console.groupEnd();
-
